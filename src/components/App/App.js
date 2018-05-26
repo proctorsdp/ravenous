c4a73f9923480fdfd8464f24b56177935cdaa36b
Steven Proctor <proctor@stevens-mbp.attlocal.net>
2018-05-26 14:02:15 -0400

    Added state to the App class. Added searchYelp method.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,39 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
+import Yelp from '../../util/Yelp';
 
-let business = {
-    imageSrc: "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
-    name: "MarginOtto Pizzeria",
-    address: "1010 Paddington Way",
-    city: "Flavortown",
-    state: "NY",
-    zipCode: "10101",
-    category: "Italian",
-    rating: 4.5,
-    reviewCount: 90
-};
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            businesses: []
+        };
+        this.searchYelp = this.searchYelp.bind(this);
+    }
 
-const businesses = [
-    business,
-    business,
-    business,
-    business,
-    business,
-    business
-];
+    searchYelp(term, location, sortBy) {
+        Yelp.search(term, location, sortBy).then(businesses => {
+            this.setState({ businesses: businesses });
+        });
+    }
 
-class App extends Component {
-  render() {
-    return (
-        <div className="App">
-            <h1>ravenous</h1>
-            <SearchBar/>
-            <BusinessList businesses={businesses}/>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>ravenous</h1>
+                <SearchBar searchYelp={this.searchYelp}/>
+                <BusinessList businesses={this.state.businesses}/>
+            </div>
+        );
+    }
 }
 
 export default App;
